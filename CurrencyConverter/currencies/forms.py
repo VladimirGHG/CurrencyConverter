3bc6104d5195c93ad8d55213345067e701ed204f
VladimirGHG <vlad2005garag@Gmail.com>
2024-08-18 16:52:49 +0400

    Main page

--- CurrencyConverter/currencies/forms.py
+++ CurrencyConverter/currencies/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class EnterCurrencies(forms.Form):
-    currency1 = forms.CharField(max_length=10)
-    currency2 = forms.CharField(max_length=10)
-    amount = forms.IntegerField(help_text="Enter only Positive Integers")
+    currency1 = forms.CharField(max_length=10, initial="USD")
+    currency2 = forms.CharField(max_length=10, initial="EUR")
+    amount = forms.IntegerField()
--- CurrencyConverter/currencies/views.py
+++ CurrencyConverter/currencies/views.py
@@ -5,4 +5,4 @@ from .forms import EnterCurrencies
 
 def home_view(request):
     context = {'form': EnterCurrencies()}
-    return render(request, "amin_converter.html", context)
+    return render(request, "main_converter.html", context)
